27c73a7dbfa952a0d1fb882046c22edf67fe920e
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-06-13 13:01:04 -0600

    fix?

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on("message", async message => {
   if (message.content === 'Gross.') {
     const emoji = message.guild.emojis.cache.find(emoji => emoji.name === 'gnome_barfing');
     console.log(emoji);
-    message.reply(`:gnome_barfing:`)
+    message.channel.send('712468402421104700')
     // message.reply(`Indubitably.`)
   }
 
