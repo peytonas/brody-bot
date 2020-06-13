a6cea83e11267a04ad9ae33d0e74b98d05e947d5
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-06-13 16:49:48 -0600

    fix

--- index.js
+++ index.js
@@ -44,7 +44,11 @@ bot.on("message", async message => {
   if (message.author.bot) return;
 
   if (message.content === `${prefix}`) {
-    message.channel.send(`!join: If user is in a voice channel, Chalooby-Bot will stream a YouTube playlist.`)
+    message.channel.send(
+      `
+      Options:
+      !join: If user is in a voice channel, Chalooby-Bot will stream a YouTube playlist.
+      `)
   }
 
   if (message.content.includes('ping') || message.content.includes('Ping')) {
