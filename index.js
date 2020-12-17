6a73a2b25220a76bec01fa02c0e3b4a7460f0177
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-17 16:16:20 -0700

    test

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const ytdl = require("ytdl-core");
 const puppeteer = require("puppeteer");
 let imgs;
 
-// const test = async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto("https://inspirobot.me/");
-//   await page.click(".btn-text");
-//   await page.waitFor(1500);
-//   imgs = await page.$$eval(".generator img[src]", (imgs) =>
-//     imgs.map((img) => img.getAttribute("src"))
-//   );
-//   console.log(imgs[0]);
-//   await browser.close();
-// };
+const test = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://inspirobot.me/");
+  await page.click(".btn-text");
+  await page.waitFor(1500);
+  imgs = await page.$$eval(".generator img[src]", (imgs) =>
+    imgs.map((img) => img.getAttribute("src"))
+  );
+  await browser.close();
+};
 
 bot.on("message", async (message) => {
   if (message.content === "!quote") {
+    await test()
     // imgs = await test();
     console.log("finding pics");
     console.log(imgs);
     const embed = new Discord.MessageEmbed()
-      .setTitle("INSPIROBOT")
-      .setThumbnail(
-        "https://inspirobot.me/website/images/inspirobot-dark-green.png"
+    .setTitle("INSPIROBOT")
+    .setThumbnail(
+      "https://inspirobot.me/website/images/inspirobot-dark-green.png"
       )
       .setColor(0xff0000)
       .setImage(
