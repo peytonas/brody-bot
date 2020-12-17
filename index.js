0bd94c0e7cdc5b457be648cff07650dc1d838f81
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-17 15:53:05 -0700

    test

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const ytdl = require("ytdl-core");
 const puppeteer = require("puppeteer");
 let imgs;
 
-const test = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://inspirobot.me/");
-  await page.click(".btn-text");
-  await page.waitFor(1500);
-  imgs = await page.$$eval(".generator img[src]", (imgs) =>
-    imgs.map((img) => img.getAttribute("src"))
-  );
-  console.log(imgs[0]);
-  await browser.close();
-};
+// const test = async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto("https://inspirobot.me/");
+//   await page.click(".btn-text");
+//   await page.waitFor(1500);
+//   imgs = await page.$$eval(".generator img[src]", (imgs) =>
+//     imgs.map((img) => img.getAttribute("src"))
+//   );
+//   console.log(imgs[0]);
+//   await browser.close();
+// };
 
 bot.on("message", async (message) => {
   if (message.content === "!quote") {
-    imgs = await test();
+    // imgs = await test();
     console.log("finding pics");
     console.log(imgs);
     const embed = new Discord.MessageEmbed()
