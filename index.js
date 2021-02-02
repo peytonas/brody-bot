6a9a78e6da9850117db0c94d29961fdacba4b1e4
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-02-02 11:10:02 -0700

    package fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const request = require("request");
 
 const prefix = process.env.PREFIX;
 const token = process.env.BOT_TOKEN;
