0878253f62e343dfe3837aa3803b0a750591d5d0
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-02-03 16:53:20 -0700

    new jokes

--- index.js
+++ index.js
@@ -74,7 +74,21 @@ let grossGifs = [
 
 let jokes = [
   "I saw yo mama fall down the other day. It wasn't that funny to me...||But the sidewalk cracked up!||",
-  "I'm not very funny so I have no idea. ||Who am I kidding, I'm hilarious.||"
+  "I'm not very funny so I have no idea. ||Who am I kidding, I'm hilarious.||",
+  "What did the duck say when he found an avocado? ||Guac, guac!||",
+  "Shout out to the people who don't know what the opposite of in is!",
+  "What do you call a talking turtle? ||Fictional.||",
+  "Are we ever going to address the fact that Mr Krabs lives in Bikini Bottom?",
+  "What do you call a dog with no legs? ||Doesn't matter what you call him, he's not coming.||",
+  "I had an interview for a new job earlier today. They told me I'd be earning $20,000 now, and later my pay will increase to $40,000. I told the lady I'll just come back later.",
+  "I hate when my wife asks if I'm listening to her. ||What a rude way to start a conversation.||",
+  "I asked my cat what two minus two is. ||He said nothing.||",
+  "A Spanish magician says he'll disappear on the count of three. Uno...Dos...||He disappeared without a tres.||",
+  "My wife asked me to stop singing Wonderwall all the time. ||I said maybe.||",
+  "There have been rumors Chuck Norris' tears can cure COVID. Too bad he's never cried.",
+  "Chuck Norris had sex with Optimus Prime and invented dubstep.",
+  "Chuck Norris once urinated in a semi truck's gas tank. That truck is now known as Optimus Prime.",
+  "Chuck Norris doesn't flush the toilet, he scares the sh*t out of it."
 ]
 
 function getRandomInt(max) {
@@ -204,7 +218,7 @@ bot.on("message", async (message) => {
     message.content.includes("Tell me a joke") ||
     message.content.includes("tell me a joke")
   ) {
-    let i = getRandomInt(2);
+    let i = getRandomInt(16);
     message.channel.send("Let's see...")
     setTimeout(function () {
       message.channel.send(jokes[i]);
