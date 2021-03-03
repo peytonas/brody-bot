78307c366e28decf4ec8488224a700624b9bc022
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-03-03 15:29:32 -0700

    test

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ bot.on("message", async (message) => {
     }
   }
 
-  if (lowerCase.includes(` tea `))
+  if (lowerCase.includes(` tea `) || lowerCase.includes(` tea`) || lowerCase.includes(`tea `))
   {
     if (message.author.bot) {
       return;
